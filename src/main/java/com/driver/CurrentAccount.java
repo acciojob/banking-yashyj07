04ea1dbe49245fc1fb1361ab53ad4913fb8fc60b
Yash Jivani <yashyj1908@gamil.com>
2023-08-22 18:33:43 +0530

    Attempt 2

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import java.util.PriorityQueue;
+
 public class CurrentAccount extends BankAccount{
     private String tradeLicenseId; //consists of Uppercase English characters only
 
@@ -30,53 +32,60 @@ public class CurrentAccount extends BankAccount{
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
-        // If it is not possible, throw "Valid License can not be generated" Exception
-        String licenseId=getTradeLicenseId();
-        char[] licenseChars = licenseId.toCharArray();
-
-        int n = licenseChars.length;
-        boolean rearranged = false;
-
-        for (int i = 1; i < n; i++) {
-            if (licenseChars[i] == licenseChars[i - 1]) {
-                rearranged = true;
-                break;
+        // If it is not possible, throw "Valid License can not be generated" Exception'
+        char[]arr=tradeLicenseId.toCharArray();
+        if(isValid(arr)) return;
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->{
+            return b.freq-a.freq;
+        });
+        int[] freq =new int[26];
+        int n=arr.length;
+        for (char c : arr) {
+            freq[c - 'A']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                pq.add(new Pair((char) (i + 'A'), freq[i]));
             }
         }
-
-        if (!rearranged) {
-            return; // The license is already valid, no need to rearrange
-        }
-
-        // Rearrange characters to create a valid license
-        for (int i = 1; i < n; i += 2) {
-            if (i + 1 < n && licenseChars[i] == licenseChars[i + 1]) {
-                // Swap characters at i and i+1 to break consecutive pattern
-                char temp = licenseChars[i];
-                licenseChars[i] = licenseChars[i - 1];
-                licenseChars[i - 1] = temp;
+        StringBuilder sb=new StringBuilder();
+        while(!pq.isEmpty()){
+            Pair p1=pq.remove();
+            sb.append(p1.c);
+            p1.freq--;
+            boolean flag=false;
+            if(!pq.isEmpty()){
+                Pair p2=pq.remove();
+                sb.append(p2.c);
+                p2.freq--;
+                flag=true;
+                if(p1.freq>0)pq.add(p1);
+                if(p2.freq>0)pq.add(p2);
             }
+            if(flag) continue;
+            if(p1.freq>0)pq.add(p1);
         }
-
-        // Now the license is rearranged and valid
-        String rearrangedLicense = new String(licenseChars);
-
-        if (!isValidLicense(rearrangedLicense)) {
-            throw new Exception("Valid License can not be generated");
-        }
-
-        setTradeLicenseId(rearrangedLicense);
+        String id=sb.toString();
+        char[]newArr=id.toCharArray();
+        if(isValid(newArr)) this.tradeLicenseId=id;
+        else throw new Exception("Valid License can not be generated");
     }
-    private boolean isValidLicense(String licenseId) {
-        char[] licenseChars = licenseId.toCharArray();
+    private boolean isValid(char[]arr){
+        int n=arr.length;
+        for(int i=1;i<n;i++){
+            if(arr[i]==arr[i-1]) return false;
+        }
+        return true;
+    }
+    static class Pair{
+        char c;
+        int freq;
 
-        for (int i = 1; i < licenseChars.length; i++) {
-            if (licenseChars[i] == licenseChars[i - 1]) {
-                return false;
-            }
+        public Pair(char c, int freq) {
+            this.c = c;
+            this.freq = freq;
         }
 
-        return true;
     }
 
 }
