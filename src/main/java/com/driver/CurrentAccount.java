5955aa8a651b7bee5e554813af99a3bf818ed5fb
Yash Jivani <yashyj1908@gamil.com>
2023-08-22 18:54:48 +0530

    Attempt 6

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -9,6 +9,9 @@ public class CurrentAccount extends BankAccount{
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name, balance, 5000);
+        if(balance<5000) {
+            throw new Exception("Insufficient Balance");
+        }
         this.tradeLicenseId = tradeLicenseId;
     }
 
